244e9f50c4a838272ae5d6593130636dcec352ba
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-24 00:09:32 +0300

    js

---
+++ reward.js
@@ -0,0 +1,9 @@
+let age = 100;
+
+if (age < 100){
+    console.log("Not eligible");
+}else if (age === 100){
+    console.log("Eligible");
+}else{
+    console.log("Eligible");
+}
---
+++ scrimba.js
@@ -0,0 +1,18 @@
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+
+let hasBlackJack = fasle;
+let isAlive = true;
+
+if (sum <= 20){
+    console.log("Not eligible");
+}else if (sum === 21){
+    console.log("Wohoo! You've got BlackJack");
+    hasBlackJack = true;
+}else{
+    console.log("You're out of the game!");
+    isAlive = false;
+}
+
+console.log(isAlive)
