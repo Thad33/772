192c12a1bdab7b3a84ecc58c8dbd5b7244f951ca
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-23 22:24:08 +0300

    Js concatination

--- script.js
+++ script.js
@@ -5,10 +5,20 @@
 //function  myLogger() {
     //console.log(20)
 //}
-let countEl = document.getElementById("count-El");
-console.log(count);
+let countEl = document.getElementById("count-el");
+//console.log(count);
 let count = 0;
-function increament(){
+function increment(){
     count = count + 1;
     countEl.innerText = count;
 }
+ function save(){
+    console.log()
+ }
+ //screen concatination
+ let messageToUser = "We have loged";
+ //
+ let username = "per";
+ let message = "You have tree new notification";
+
+ console.log(user + "," + username + "!" )
