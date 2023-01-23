26742950e4c3a1318ac75bce1bc4b604b2c8041b
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-23 21:41:40 +0300

    js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@
 //function  myLogger() {
     //console.log(20)
 //}
+let countEl = document.getElementById("count-El");
+console.log(count);
+let count = 0;
+function increament(){
+    count = count + 1;
+    countEl.innerText = count;
+}
