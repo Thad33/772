6bc671a8a620be85bb8c7e0df3f518767a9e00fa
Thaddaeus <thaddaeusotieno@gmail.com>
2023-01-25 14:48:16 +0300

    Bolean expressions in Js

--- task.js
+++ task.js
@@ -2,5 +2,12 @@
 console.log(5)
 console.log(5>3) //True
 console.log(5<3)
-console.log(8<)
-console.log(3<3)
+console.log(8<=8)
+console.log(3<=3)
+//Arithmetics
+console.log(2 + 3) // Addition
+console.log(3 - 2) // Subtraction
+console.log(2 * 3) // Multiplication
+console.log(3 / 2) // Division
+console.log(3 % 2) // Modulus - finding remainder
+console.log(3 ** 2) // Exponentiation 3 ** 2 == 3 * 3
